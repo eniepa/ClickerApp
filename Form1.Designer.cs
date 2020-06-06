599b22a9d555403afe3a4c54f8d5eb74e92ba61e
eniepa <apeine.k@gmail.com>
2020-06-06 18:47:00 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ClickerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstButton
+            // 
+            this.FirstButton.BackColor = System.Drawing.Color.LightCoral;
+            this.FirstButton.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstButton.Location = new System.Drawing.Point(188, 100);
+            this.FirstButton.Name = "FirstButton";
+            this.FirstButton.Size = new System.Drawing.Size(200, 200);
+            this.FirstButton.TabIndex = 0;
+            this.FirstButton.Text = "Click Me";
+            this.FirstButton.UseVisualStyleBackColor = false;
+            this.FirstButton.Click += new System.EventHandler(this.FirstButton_Click);
+            this.FirstButton.MouseEnter += new System.EventHandler(this.FirstButton_MouseEnter);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(661, 340);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(100, 45);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            this.CloseButton.MouseEnter += new System.EventHandler(this.CloseButton_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.FirstButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FirstButton;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickerApp
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        int fontSize = 20;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstButton_Click(object sender, EventArgs e)
+        {
+            FirstButton.BackColor = Color.Pink;
+            FirstButton.Width -= 5;
+            FirstButton.Height -= 5;
+            FirstButton.Top = rand.Next(0, 300);
+            FirstButton.Left = rand.Next(0, 300);
+            int red = rand.Next(0, 256);
+            int green = rand.Next(0, 256);
+            int blue = rand.Next(0, 256);
+            FirstButton.BackColor = Color.FromArgb(red, green, blue);
+            FirstButton.Font = new Font("Arial", fontSize, FontStyle.Bold);
+            fontSize -= 1;
+            if (fontSize != 1)
+            {
+                fontSize -= 1;
+            }
+        }
+
+        private void CloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            CloseButton.Top = rand.Next(0, 300);
+            CloseButton.Left = rand.Next(0, 300);
+
+        }
+
+        private void FirstButton_MouseEnter(object sender, EventArgs e)
+        {
+            FirstButton.Top = rand.Next(0, 300);
+            FirstButton.Left = rand.Next(0, 300);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
